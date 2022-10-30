3f4a94e0d90997147016d91d625ae313617ce616
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-30 18:12:17 +0300

    Refactoring assignment completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,14 @@ class Artist
 
   @@artists = []
 
+  # name spacing
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
@@ -19,13 +25,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +42,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
